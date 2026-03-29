877674731bb103702bd7ed8a07cc4ea35fe05fa5
Jean Sousa <jeansousa2001@gmail.com>
2026-03-29 16:36:22 -0300

    feat: add insertion logic and overflow metrics

--- Trabalho1/src/main.py
+++ Trabalho1/src/main.py
@@ -10,23 +10,41 @@ class Menu:
         self.show_menu()
 
     def show_menu(self):
-        print("=" * 60 + "\n")
-        print("Qual operação deseja realizar?\n")
-        print("[1] Visualizar árvore atual\n" + 
-              "[2] Inserir nó\n" + 
-              "[3] Remover nó" + "\n" + 
-              "[4] Buscar nó\n" + 
-              "[5] Sair\n")
-        print("\n" + "=" * 60)
-        option = input("Digite a opção desejada: ")
+        while True:
+            print("=" * 60 + "\n")
+            print("Qual operação deseja realizar?\n")
+            print("[1] Visualizar árvore atual\n" + 
+                  "[2] Inserir nó\n" + 
+                  "[3] Remover nó" + "\n" + 
+                  "[4] Buscar nó\n" + 
+                  "[5] Exibir Métricas de Overflow\n" +
+                  "[6] Sair\n")
+            print("\n" + "=" * 60)
+            option = input("Digite a opção desejada: ")
 
-        if option == "1":
-            self.arvore.get_tree()
-        # elif option == "2":
-        elif option == "3":
-            key = input("Qual chave deseja remover?\n")
-            self.arvore.remove(key)
-        elif option == "4":
-            key = input("Qual chave deseja buscar?\n")
-            tuple = self.arvore.search(key)
-            print(tuple)
+            if option == "1":
+                self.arvore.get_tree()
+                
+            elif option == "2":
+                key = input("Digite a chave para inserir: ")
+                # Valor gerado automaticamente no padrão "RX"
+                value = f"R{key}"
+                self.arvore.insert(key, value)
+                print(f"Sucesso: Nó ({key}, '{value}') processado!")
+                
+            elif option == "3":
+                key = input("Qual chave deseja remover?\n")
+                self.arvore.remove(int(key))
+                
+            elif option == "4":
+                key = input("Qual chave deseja buscar?\n")
+                tuple_rec = self.arvore.search(int(key))
+                print(tuple_rec)
+                
+            elif option == "5":
+                print(f"Páginas de overflow: {self.arvore.count_overflow_pages()}")
+                print(f"Tamanho médio das cadeias: {self.arvore.average_overflow_length()}")
+                
+            elif option == "6":
+                print("Encerrando...")
+                break
